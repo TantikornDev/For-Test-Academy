534bce5c03dcd5df6f44e8c3e163a87b3ffefc07
mootamDev <>
2022-10-21 22:48:53 +0700

    [ADD] commit 2

--- index.js
+++ index.js
@@ -4,12 +4,14 @@
 const http = require('http');
 const port = 8021;
 const requestHandler = (request, response) => {
-    console.log(request.url);
+    console.log('check <<<<<', request.url);
     response.end('Hello Node.js Server!');
 };
 // section2:
 const server = http.createServer(requestHandler);
 server.listen(port, (err) => {
-    if (err) { return console.log('Nong Kai Error', err); }
+    if (err) { 
+        return console.log('Nong Kai Error', err); 
+    }
     console.log(`server is listening on ${port}`);
 });
